41f12e39234cfa159215f59ce12b3e4baaa70ecd
oinke <email.oinke@gmail.com>
2019-03-10 00:02:57 +0800

    profile cover upload

--- src/components/main-profile.js
+++ src/components/main-profile.js
@@ -105,35 +105,39 @@ class MainProfile extends ReduxMixin(PolymerElement) {
         .gap {
           margin-right: 24px;
         }
+        .clicky {
+          margin: 0px;
+        }
       </style>
       <app-location route="{{route}}" url-space-regex="^[[rootPath]]"></app-location>
         <div class="header">
           <div class="cover-image">
             <div class="cover-header">
               <div class="flex"></div>
-              <div class="cover-button">
-               
-              <svg width="28px" height
-                cursor: pointer;="28px" viewBox="0 0 28 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <defs>
-                        <path d="M14,2 C8.5,2 4,6.5 4,12 C4,17.5 8.5,22 14,22 C19.5,22 24,17.5 24,12 C24,6.5 19.5,2 14,2 Z M19,12.8 C19,12.9 18.9,13 18.8,13 L15,13 L15,16.8 C15,16.9 14.9,17 14.8,17 L13.2,17 C13.1,17 13,16.9 13,16.8 L13,13 L9.2,13 C9.1,13 9,12.9 9,12.8 L9,11.2 C9,11.1 9.1,11 9.2,11 L13,11 L13,7.2 C13,7.1 13.1,7 13.2,7 L14.8,7 C14.9,7 15,7.1 15,7.2 L15,11 L18.8,11 C18.9,11 19,11.1 19,11.2 L19,12.8 Z" id="path-1"></path>
-                        <filter x="-35.0%" y="-25.0%" width="170.0%" height="170.0%" filterUnits="objectBoundingBox" id="filter-2">
-                            <feOffset dx="0" dy="2" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
-                            <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
-                            <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.5 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
-                        </filter>
-                    </defs>
-                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <g id="Shape" fill-rule="nonzero">
-                            <use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
-                            <use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+
+              <label for="image" class="clicky">
+                <input type="file" name="image" id="image" style="display:none;" accept="image/gif, image/jpeg, image/png" on-change="_resize" value="{{file::input}}"/>
+                  <div class="cover-button">
+                  <svg width="28px" height
+                    cursor: pointer;="28px" viewBox="0 0 28 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <defs>
+                            <path d="M14,2 C8.5,2 4,6.5 4,12 C4,17.5 8.5,22 14,22 C19.5,22 24,17.5 24,12 C24,6.5 19.5,2 14,2 Z M19,12.8 C19,12.9 18.9,13 18.8,13 L15,13 L15,16.8 C15,16.9 14.9,17 14.8,17 L13.2,17 C13.1,17 13,16.9 13,16.8 L13,13 L9.2,13 C9.1,13 9,12.9 9,12.8 L9,11.2 C9,11.1 9.1,11 9.2,11 L13,11 L13,7.2 C13,7.1 13.1,7 13.2,7 L14.8,7 C14.9,7 15,7.1 15,7.2 L15,11 L18.8,11 C18.9,11 19,11.1 19,11.2 L19,12.8 Z" id="path-1"></path>
+                            <filter x="-35.0%" y="-25.0%" width="170.0%" height="170.0%" filterUnits="objectBoundingBox" id="filter-2">
+                                <feOffset dx="0" dy="2" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                                <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                                <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.5 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+                            </filter>
+                        </defs>
+                        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g id="Shape" fill-rule="nonzero">
+                                <use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
+                                <use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+                            </g>
                         </g>
-                    </g>
-                </svg>
-
-              </div>
+                    </svg>
+                  </div>
+              </label>
             </div>
-
           </div>
           <div class="cover-layout">
               <div>
@@ -195,9 +199,86 @@ class MainProfile extends ReduxMixin(PolymerElement) {
     };
   }
 
+  _resize() {
+    const file = this.shadowRoot.querySelector('#image').files[0];
+    if (file.type.match(/image.*/)) {
+      const reader = new FileReader();
+      reader.onload = (readerEvent) => {
+        const image = new Image();
+        image.onload = (imageEvent) => {
+          const canvas = document.createElement('canvas');
+          const maxSize = 1160;
+          let width = image.width;
+          let height = image.height;
+          if (width > height) {
+            if (width > maxSize) {
+              height *= maxSize / width;
+              width = maxSize;
+            }
+          } else {
+            if (height > maxSize) {
+              width *= maxSize / height;
+              height = maxSize;
+            }
+          }
+          canvas.width = width;
+          canvas.height = height;
+          canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+          const dataUrl = canvas.toDataURL('image/jpeg');
+          const resizedImage = this._dataURLToBlob(dataUrl);
+          this._upload(resizedImage);
+        };
+        image.src = readerEvent.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  _upload(file) {
+    const formData = new FormData();
+    formData.append('image', file);
+    const token = localStorage.getItem('jwt');
+    const url = `${this.env.apiUrl}/users/profile/upload-pfc/`;
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+      headers: {'Authorization': `Bearer ${token}`},
+    })
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          this.random = Math.floor(Math.random() * 9000000000) + 1000000000;
+
+          this.dispatchAction({
+            type: 'CHANGE_USERID',
+            userid: this.userid,
+          });
+        })
+        .catch((error) => console.log('Error:', error));
+  }
+
+  _dataURLToBlob(dataURL) {
+    const BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+      const parts = dataURL.split(',');
+      const contentType = parts[0].split(':')[1];
+      const raw = parts[1];
+      return new Blob([raw], {type: contentType});
+    }
+    const parts = dataURL.split(BASE64_MARKER);
+    const contentType = parts[0].split(':')[1];
+    const raw = window.atob(parts[1]);
+    const rawLength = raw.length;
+    const uInt8Array = new Uint8Array(rawLength);
+    for (let i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+    return new Blob([uInt8Array], {type: contentType});
+  }
   _language(e) {
     this.txt = translations[this.language];
   }
+
   _editProfile() {
     this.set('route.path', './settings/');
   }

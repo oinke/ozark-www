91a0f7166ab15ec3df05cf6b8c744e88074cb60b
oinke <email.oinke@gmail.com>
2019-03-12 10:13:57 +0800

    last seen

--- src/components/navigation/site-search.js
+++ src/components/navigation/site-search.js
@@ -161,6 +161,22 @@ class SiteSearch extends ReduxMixin(PolymerElement) {
     };
   }
 
+  _lastSeen = function(epochTime) {
+    let lastSeen = 'Last seen: ';
+    const currentTime = Date.now();
+    const elapsed = currentTime - epochTime;
+    const toMins = (mins) => Math.floor((mins/1000)/60);
+    const toHours = (hours) => Math.floor(toMins(hours)/60);
+    const toDays = (days) => Math.floor(toHours(days)/24);
+    elapsed < 120000 ? lastSeen += 'moments ago'
+    : elapsed < 3600000 ? lastSeen += toMins(elapsed) + ' minutes ago'
+    : elapsed < 7000000 ? lastSeen += '1 hour ago'
+    : elapsed < 86400000 ? lastSeen += toHours(elapsed) + ' hours ago'
+    : elapsed < 172000000 ? lastSeen += '1 day ago'
+    : lastSeen += toDays(elapsed) + ' days ago';
+    return lastSeen;
+  }
+
   _close() {
     this.updateStyles({'--show-search': 'none'});
   }

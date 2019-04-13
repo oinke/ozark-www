82c29a19840043d3ac413105c9f42639d02ae0f0
jrd <gh18.x.jrd@xoxy.net>
2019-04-13 04:45:02 +0000

    copyedit, remove Followers

--- src/routes/help/page-use.js
+++ src/routes/help/page-use.js
@@ -61,8 +61,8 @@ class PageUse extends ReduxMixin(PolymerElement) {
     <section>
         <header>
             <h1>Acceptable Use Policy</h1></header>
-        <p>[[env.siteName]] is a web-based platform ([[env.webAddress]]) (“Site”) offering a suite of tools for users (“users”) to optimize their experience and their engagement and transactions with their followers (“Followers”) (“Service”). To preserve the positive experience of all our users, Followers and visitors (collectively, “Users”), you agree that you will use the Service only in a manner consistent with the following Acceptable Use Policy.</p>
-        <p><strong>You agree not to use the Service to post or display User Content that:</strong></p>
+        <p>[[env.siteName]] is a web-based platform ([[env.webAddress]]) (“Site”) offering a suite of tools for users (“users”) to optimize their experience. To preserve the positive experience of all our users, Followers and visitors (collectively, “Users”), you agree that you will use the Site only in a manner consistent with the following Acceptable Use Policy.</p>
+        <p><strong>You agree not to use the Site to post or display User Content that:</strong></p>
         <ul>
             <li>Creates a risk of harm, loss, physical or mental injury, emotional distress, death, disability, disfigurement, or physical or mental illness to yourself, to any other person, or to any animal;</li>
             <li>May create a risk of any other loss or damage to any person or property;</li>
@@ -76,21 +76,21 @@ class PageUse extends ReduxMixin(PolymerElement) {
         </ul>
         <p><strong>You agree not to engage in any of the following prohibited activities:</strong></p>
         <ul>
-            <li>Use, display, mirror or frame the Service, any individual element within the Service, the [[env.siteName]] name, trademark, logo or other proprietary information, or the layout and design of any page, without our express written consent;</li>
-            <li>Access, tamper with, or use non-public areas of the Service, our computer systems, or the technical delivery systems of our providers;</li>
+            <li>Use, display, mirror or frame the Site, any individual element within the Site, the [[env.siteName]] name, trademark, logo or other proprietary information, or the layout and design of any page, without our express written consent;</li>
+            <li>Access, tamper with, or use non-public areas of the Site, our computer systems, or the technical delivery systems of our providers;</li>
             <li>Attempt to probe, scan, or test the vulnerability of any [[env.siteName]] system or network or breach any security or authentication measures;</li>
-            <li>Avoid, bypass, remove, deactivate, impair, descramble or otherwise circumvent any technological measure implemented by [[env.siteName]] or any of our providers or any other third party (including another user) to protect the Service or [[env.siteName]] Content;</li>
-            <li>Attempt to access or search the Service, User Content or [[env.siteName]] Content or scrape or download User Content or [[env.siteName]] Content from the Service, or otherwise use, upload content to, or create new links, reposts, or referrals in the Service through the use of any engine, software, tool, agent, device or mechanism (including automated scripts, spiders, robots, crawlers, data mining tools or the like) other than the software and/or search agents provided by [[env.siteName]] or other generally available third party web browsers;</li>
-            <li>Use any meta tags or other hidden text or metadata utilizing a [[env.siteName]] or [[env.siteName]] trademark, logo, URL, or product name without [[env.siteName]]’s express written consent;</li>
-            <li>Use the Service for any commercial purpose or the benefit of any third party, except as otherwise explicitly permitted for you by [[env.siteName]] or in any manner not permitted by the Terms;</li>
-            <li>Attempt to decipher, decompile, disassemble or reverse engineer any of the software used to provide the Service;</li>
-            <li>Interfere with, or attempt to interfere with, the access of any user, host or network, including, without limitation, sending a virus, overloading, flooding, spamming, or mail-bombing the Service;</li>
-            <li>Collect or store any personally identifiable information from the Service from other users of the Service without their express permission;</li>
+            <li>Avoid, bypass, remove, deactivate, impair, descramble or otherwise circumvent any technological measure implemented by [[env.siteName]] or any of our providers or any other third party (including another user) to protect the Site or [[env.siteName]] Content;</li>
+            <li>Attempt to access or search the Site, User Content or [[env.siteName]] Content or scrape or download User Content or [[env.siteName]] Content from the Site, or otherwise use, upload content to, or create new links, reposts, or referrals in the Site through the use of any engine, software, tool, agent, device or mechanism (including automated scripts, spiders, robots, crawlers, data mining tools or the like) other than the software and/or search agents provided by [[env.siteName]] or other generally available third party web browsers;</li>
+            <li>Use any meta tags or other hidden text or metadata utilizing [[env.siteName]] or the [[env.siteName]] trademark, logo, URL, or product name without [[env.siteName]]’s express written consent;</li>
+            <li>Use the Site for any commercial purpose or the benefit of any third party, except as otherwise explicitly permitted for you by [[env.siteName]] or in any manner not permitted by the Terms;</li>
+            <li>Attempt to decipher, decompile, disassemble or reverse engineer any of the software used to provide the Site;</li>
+            <li>Interfere with, or attempt to interfere with, the access of any user, host or network, including, without limitation, sending a virus, overloading, flooding, spamming, or mail-bombing the Site;</li>
+            <li>Collect or store any personally identifiable information from the Site from other users of the Site without their express permission;</li>
             <li>Impersonate or misrepresent your affiliation with any person or entity;</li>
             <li>Violate any applicable law or regulation; or</li>
             <li>Encourage or enable any other individual to do any of the activities prohibited in this Acceptable Use Policy.</li>
         </ul>
-        <p>[[env.siteName]] reserves the right, but is not obligated, to remove any User Content for any reason or for no reason, including User Content that [[env.siteName]] believes violates this Acceptable Use Policy or its Terms of Service. [[env.siteName]] may also permanently or temporarily terminate or suspend a User’s access to the Service without notice and liability for any reason, including if, in [[env.siteName]]’s sole determination, a User violates any provision of this Acceptable Use Policy , our <a href="/help/terms">Terms of Service</a>, or for no reason.</p>
+        <p>[[env.siteName]] reserves the right, but is not obligated, to remove any User Content for any reason or for no reason, including User Content that [[env.siteName]] believes violates this Acceptable Use Policy or its Terms of Service. [[env.siteName]] may also permanently or temporarily terminate or suspend a User’s access to the Site without notice and liability for any reason, including if, in [[env.siteName]]’s sole determination, a User violates any provision of this Acceptable Use Policy , our <a href="/help/terms">Terms of Service</a>, or for no reason.</p>
     </section>
 </article>
           </div>
